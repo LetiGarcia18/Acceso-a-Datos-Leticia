722b4ebcb57e4fc8b670eec0869484b8baca6378
LetiGarcia18 <leticiagarciagonzalez18@gmail.com>
2022-11-11 13:06:43 +0100

    Creado el proyecto de la práctica 1

---
+++ practica1.dao.leticia/src/dao/InterfazDao.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.util.ArrayList;
+
+public interface InterfazDao<T> {
+	
+	/**
+	 * Inserta en objeto T recibido en la base de datos.
+	 * @param t Un objeto T
+	 */
+	public void insertar(T t);
+	/**
+	 * Modifica un objeto T de la base de datos.
+	 * @param t Un objeto T.
+	 */
+	public void modificar(T t);
+	/**
+	 * Elimina un objeto T de la base de datos
+	 * @param t
+	 */
+	public void borrar(T t);
+	/**
+	 * Muestra todos los objetos T de la base de datos.
+	 * @return ArrayList de objetos T.
+	 */
+	public ArrayList<T> buscarTodos();
+	/**
+	 * Muestra el objeto T con el id especificado.
+	 * @param i El id del objeto T
+	 * @return Un objeto T
+	 */
+	public T buscarPorId(int i);
+
+}
